ed233e7bd500a90bb4d6ea78fcac57b404970f65
MrIYoes <steffen.brauer@gmx.net>
2012-06-17 21:35:54 +0200

    MC on-policy angefangen

--- RLGlueJava/src/examples/mines/P4Cm4nAgentSteffen.java
+++ RLGlueJava/src/examples/mines/P4Cm4nAgentSteffen.java
@@ -1,28 +1,4 @@
 package examples.mines;
-/*
- * Copyright 2008 Brian Tanner
- * http://rl-glue-ext.googlecode.com/
- * brian@tannerpages.com
- * http://brian.tannerpages.com
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *  $Revision: 991 $
- *  $Date: 2009-02-08 17:29:20 -0700 (Sun, 08 Feb 2009) $
- *  $Author: brian@tannerpages.com $
- *  $HeadURL: http://rl-library.googlecode.com/svn/trunk/projects/packages/examples/mines-sarsa-java/SampleSarsaAgent.java $
- *
- */
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -31,7 +7,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+
 import org.rlcommunity.rlglue.codec.AgentInterface;
 import org.rlcommunity.rlglue.codec.types.Action;
 import org.rlcommunity.rlglue.codec.types.Observation;
@@ -39,27 +20,26 @@ import org.rlcommunity.rlglue.codec.util.AgentLoader;
 import org.rlcommunity.rlglue.codec.taskspec.TaskSpec;
 
 /**
-This is a very simple Sarsa agent for discrete-action, discrete-state
-environments.  It uses epsilon-greedy exploration.
+This is a very simple MC On-Policy Agent.
 
-We've made a decision to store the previous action and observation in
-their raw form, as structures.  This code could be simplified and you
-could store them just as ints.
- * @author Brian Tanner
+ * @author Steffen Brauer
  */
 public class P4Cm4nAgentSteffen implements AgentInterface {
 
     private Random randGenerator = new Random();
     private Action lastAction;
     private Observation lastObservation;
-    private double[][] valueFunction = null;
-    private double sarsa_stepsize = 0.1;
-    private double sarsa_epsilon = 0.1;
-    private double sarsa_gamma = 1.0;
+    private double[][] qFunction = null;
+    private double[][] pi = null;
+    private Set<Double> returns = null;
     private int numActions = 0;
     private int numStates = 0;
-    private boolean policyFrozen = false;
-    private boolean exploringFrozen = false;
+    private double epsilon = 0.1;
+    
+    private List<Integer> states;
+    private List<Integer> actions;
+    private List<Double>  rewards;
+    private int episodecounter;
 
     /**
      * Parse the task spec, make sure it is only 1 integer observation and
@@ -83,9 +63,9 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
         assert (!theTaskSpec.getDiscreteActionRange(0).hasSpecialMaxStatus());
         numActions = theTaskSpec.getDiscreteActionRange(0).getMax() + 1;
 
-        sarsa_gamma=theTaskSpec.getDiscountFactor();
+    //    sarsa_gamma=theTaskSpec.getDiscountFactor();
 
-        valueFunction = new double[numActions][numStates];
+      pi = new double[numActions][numStates];
 
     }
 
@@ -96,14 +76,45 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
      * @return
      */
     public Action agent_start(Observation observation) {
-        int newActionInt = egreedy(observation.getInt(0));
+    	states = new ArrayList<Integer>();
+    	actions = new ArrayList<Integer>();
+    	rewards = new ArrayList<Double>();
+   	 	this.episodecounter = 0;
+    	
+   	 	//Strategie P ist e-soft, wenn P(s,a) >= e/A(s) für alle s, a
+   	 	for(int i = 0; i < numActions ; i++){
+   	 		for(int j = 0; j < numStates ; j++){
+   	 			pi[i][j] = epsilon/numActions + randGenerator.nextDouble();
+   	 		}
+   	 	}
+        /**
+         * Initialize, for all s element S, a element A(s)
+         * Q(s,a) <- arbitrary
+         * Returns(s,a) <- empty list
+         * pi <- an arbitrary e-soft policy
+         * 
+         * Repeat forever:
+         * 	(a) Generate an episode using pi
+         *  (b) For each pair s,a appearing in the episode:
+         *  	R- return following the first occurence of s,a
+         *  	Append R to Returns(s,a)
+         *  	Q(s,a) <- average(Returns(s,a))
+         *  (c) For each s in the episode:
+         *  	a* <- arg max_a Q(s,a)
+         *  	For all a element A(s):
+         *  				1-e+e/|a(s)| if a = a*
+         *  	pi(s,a)
+         *  				e/|A(s)|     if a != a*
+         */
+    	
+    	//int newActionInt = egreedy(observation.getInt(0));
 
         /**
          * Create a structure to hold 1 integer action
          * and set the value
          */
         Action returnAction = new Action(1, 0, 0);
-        returnAction.intArray[0] = newActionInt;
+       // returnAction.intArray[0] = newActionInt;
 
         lastAction = returnAction.duplicate();
         lastObservation = observation.duplicate();
@@ -120,28 +131,18 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
      * @return
      */
     public Action agent_step(double reward, Observation observation) {
-        int newStateInt = observation.getInt(0);
-        int lastStateInt = lastObservation.getInt(0);
-        int lastActionInt = lastAction.getInt(0);
-
-        int newActionInt = egreedy(newStateInt);
-
-        double Q_sa = valueFunction[lastActionInt][lastStateInt];
-        double Q_sprime_aprime = valueFunction[newActionInt][newStateInt];
-
-        double new_Q_sa = Q_sa + sarsa_stepsize * (reward + sarsa_gamma * Q_sprime_aprime - Q_sa);
-        /*	Only update the value function if the policy is not frozen */
-        if (!policyFrozen) {
-            valueFunction[lastActionInt][lastStateInt] = new_Q_sa;
+    	this.episodecounter = this.episodecounter + 1;
+    	states.set(episodecounter, observation.getInt(0));
+    	rewards.set(episodecounter, reward);
+    	int maxIndex = 0;
+        for (int a = 1; a < numActions; a++) {
+            if (pi[a][observation.getInt(0)] > pi[maxIndex][observation.getInt(0)]) {
+                maxIndex = a;
+            }
         }
-
-        /* Creating the action a different way to showcase variety */
         Action returnAction = new Action();
-        returnAction.intArray = new int[]{newActionInt};
-
-        lastAction = returnAction.duplicate();
-        lastObservation = observation.duplicate();
-
+        returnAction.intArray = new int[]{maxIndex};
+        actions.set(episodecounter, maxIndex);
         return returnAction;
     }
 
@@ -150,27 +151,28 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
      * @param reward
      */
     public void agent_end(double reward) {
-        int lastStateInt = lastObservation.getInt(0);
-        int lastActionInt = lastAction.getInt(0);
-
-        double Q_sa = valueFunction[lastActionInt][lastStateInt];
-        double new_Q_sa = Q_sa + sarsa_stepsize * (reward - Q_sa);
-
-        /*	Only update the value function if the policy is not frozen */
-        if (!policyFrozen) {
-            valueFunction[lastActionInt][lastStateInt] = new_Q_sa;
-        }
-        lastObservation = null;
-        lastAction = null;
+    	/*(b) For each pai s,a appearing in the episode:
+            *  	R- return following the first occurence of s,a
+            *  	Append R to Returns(s,a)
+            *  	Q(s,a) <- average(Returns(s,a))
+            *  (c) For each s in the episode:
+            *  	a* <- arg max_a Q(s,a)
+            *  	For all a element A(s):
+            *  				1-e+e/|a(s)| if a = a*
+            *  	pi(s,a)
+            *  				e/|A(s)|     if a != a*
+            */
+    	 states = new ArrayList<Integer>();
+    	 actions = new ArrayList<Integer>();
+    	 rewards = new ArrayList<Double>();
+    	 this.episodecounter = 0;
     }
 
     /**
      * Release memory that is no longer required/used.
      */
     public void agent_cleanup() {
-        lastAction = null;
-        lastObservation = null;
-        valueFunction = null;
+        //alles auf null setzen
     }
 
     /**
@@ -180,73 +182,16 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
      * @return
      */
     public String agent_message(String message) {
-
-        if (message.equals("freeze learning")) {
-            policyFrozen = true;
-            return "message understood, policy frozen";
+    	if (message.equals("team name")) {
+            return "P4Cm4n";
         }
-        if (message.equals("unfreeze learning")) {
-            policyFrozen = false;
-            return "message understood, policy unfrozen";
+        if (message.equals("team members")) {
+            return "Steffen Brauer, Andre Harms, Florian Johannßen, Jan-Christoph Meier, Florian Ocker, Olaf Potratz, Torben Woggan";
         }
-        if (message.equals("freeze exploring")) {
-            exploringFrozen = true;
-            return "message understood, exploring frozen";
-        }
-        if (message.equals("unfreeze exploring")) {
-            exploringFrozen = false;
-            return "message understood, exploring unfrozen";
-        }
-        if (message.startsWith("save_policy")) {
-            String[] parts = message.split(" ");
-            saveValueFunction(parts[1]);
-            System.out.println("Saved.");
-            return "message understood, saving policy";
-        }
-        if (message.startsWith("load_policy")) {
-            String[] parts = message.split(" ");
-            loadValueFunction(parts[1]);
-            System.out.println("Loaded.");
-            return "message understood, loading policy";
-        }
-
-        return "SampleSarsaAgent(Java) does not understand your message.";
+        return "P4Cm4n(MC On-Policy) does not understand your message.";
 
     }
 
-    /**
-     *
-     * Selects a random action with probability 1-sarsa_epsilon,
-     * and the action with the highest value otherwise.  This is a
-     * quick'n'dirty implementation, it does not do tie-breaking.
-
-     * @param theState
-     * @return
-     */
-    private int egreedy(int theState) {
-    	
-        if (!exploringFrozen) {
-            if (randGenerator.nextDouble() <= sarsa_epsilon) {
-                return randGenerator.nextInt(numActions);
-            }
-        }
-
-        /*otherwise choose the greedy action*/
-        int maxIndex = 0;
-        for (int a = 1; a < numActions; a++) {
-            if (valueFunction[a][theState] > valueFunction[maxIndex][theState]) {
-                maxIndex = a;
-            }
-        }
-        return maxIndex;
-    }
-
-    /**
-     * This is a trick we can use to make the agent easily loadable.  Using this
-     * trick you can directly execute the class and it will load itself through
-     * AgentLoader and connect to the rl_glue server.
-     * @param args
-     */
     public static void main(String[] args) {
         AgentLoader theLoader = new AgentLoader(new P4Cm4nAgentSteffen());
         theLoader.run();
@@ -262,7 +207,7 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
             DataOutputStream DO = new DataOutputStream(new FileOutputStream(new File(theFileName)));
             for (int a = 0; a < numActions; a++) {
                 for (int s = 0; s < numStates; s++) {
-                    DO.writeDouble(valueFunction[a][s]);
+                    DO.writeDouble(pi[a][s]);
                 }
             }
             DO.close();
@@ -284,7 +229,7 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
             DataInputStream DI = new DataInputStream(new FileInputStream(new File(theFileName)));
             for (int a = 0; a < numActions; a++) {
                 for (int s = 0; s < numStates; s++) {
-                    valueFunction[a][s] = DI.readDouble();
+                    pi[a][s] = DI.readDouble();
                 }
             }
             DI.close();
