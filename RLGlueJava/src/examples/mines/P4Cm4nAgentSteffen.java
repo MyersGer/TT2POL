d559735fb34385f8d1722ab27b309a3c4dc51f64
MrIYoes <steffen.brauer@gmx.net>
2012-06-18 09:24:32 +0200

    MC On-Policy gangbar gemacht

--- RLGlueJava/src/examples/mines/P4Cm4nAgentSteffen.java
+++ RLGlueJava/src/examples/mines/P4Cm4nAgentSteffen.java
@@ -27,18 +27,16 @@ This is a very simple MC On-Policy Agent.
 public class P4Cm4nAgentSteffen implements AgentInterface {
 
     private Random randGenerator = new Random();
-    private Action lastAction;
-    private Observation lastObservation;
     private double[][] qFunction = null;
     private double[][] pi = null;
-    private Set<Double> returns = null;
     private int numActions = 0;
     private int numStates = 0;
-    private double epsilon = 0.1;
+    private double epsilon = 0.9;
     
     private List<Integer> states;
     private List<Integer> actions;
     private List<Double>  rewards;
+    private List<Double>  rewards2;
     private int episodecounter;
 
     /**
@@ -66,7 +64,13 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
     //    sarsa_gamma=theTaskSpec.getDiscountFactor();
 
       pi = new double[numActions][numStates];
+      qFunction = new double[numActions][numStates];
 
+ 	 	for(int i = 0; i < numActions ; i++){
+ 	 		for(int j = 0; j < numStates ; j++){
+ 	 			pi[i][j] = epsilon/numActions + randGenerator.nextDouble();
+ 	 		}
+ 	 	}
     }
 
     /**
@@ -76,17 +80,15 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
      * @return
      */
     public Action agent_start(Observation observation) {
+    	//System.out.println("start");
     	states = new ArrayList<Integer>();
     	actions = new ArrayList<Integer>();
     	rewards = new ArrayList<Double>();
+    	rewards2 = new ArrayList<Double>();
    	 	this.episodecounter = 0;
     	
    	 	//Strategie P ist e-soft, wenn P(s,a) >= e/A(s) für alle s, a
-   	 	for(int i = 0; i < numActions ; i++){
-   	 		for(int j = 0; j < numStates ; j++){
-   	 			pi[i][j] = epsilon/numActions + randGenerator.nextDouble();
-   	 		}
-   	 	}
+   	 	
         /**
          * Initialize, for all s element S, a element A(s)
          * Q(s,a) <- arbitrary
@@ -106,18 +108,14 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
          *  	pi(s,a)
          *  				e/|A(s)|     if a != a*
          */
-    	
-    	//int newActionInt = egreedy(observation.getInt(0));
+   	 	int maxIndex = 0;
+   	 	for (int a = 1; a < numActions; a++) {
+         if (pi[a][observation.getInt(0)] > pi[maxIndex][observation.getInt(0)]) {
+             maxIndex = a;
+         }
+     }
 
-        /**
-         * Create a structure to hold 1 integer action
-         * and set the value
-         */
         Action returnAction = new Action(1, 0, 0);
-       // returnAction.intArray[0] = newActionInt;
-
-        lastAction = returnAction.duplicate();
-        lastObservation = observation.duplicate();
 
         return returnAction;
     }
@@ -131,9 +129,10 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
      * @return
      */
     public Action agent_step(double reward, Observation observation) {
+    	//System.out.println("step");
     	this.episodecounter = this.episodecounter + 1;
-    	states.set(episodecounter, observation.getInt(0));
-    	rewards.set(episodecounter, reward);
+    	states.add(observation.getInt(0));
+    	rewards.add( reward);
     	int maxIndex = 0;
         for (int a = 1; a < numActions; a++) {
             if (pi[a][observation.getInt(0)] > pi[maxIndex][observation.getInt(0)]) {
@@ -142,7 +141,7 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
         }
         Action returnAction = new Action();
         returnAction.intArray = new int[]{maxIndex};
-        actions.set(episodecounter, maxIndex);
+        actions.add( maxIndex);
         return returnAction;
     }
 
@@ -151,27 +150,68 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
      * @param reward
      */
     public void agent_end(double reward) {
+    	//System.out.println("end");
     	/*(b) For each pai s,a appearing in the episode:
             *  	R- return following the first occurence of s,a
             *  	Append R to Returns(s,a)
             *  	Q(s,a) <- average(Returns(s,a))
-            *  (c) For each s in the episode:
+            */
+    	for(int i = 0; i < states.size(); i++){
+    		double r = 0;
+    		for(int j = i; j < states.size(); j++){
+    			r = r + rewards.get(j);
+    		}
+    		rewards2.add( r);	
+    	}
+    	for(int i = 0; i< states.size(); i++){
+    		int state = states.get(i);
+    		int action = actions.get(i);
+    		int count = 0;
+    		double sum = 0;
+    		for(int j = 0; j < states.size(); j ++){
+    			if(state == states.get(j) && action == actions.get(j)){
+    				count = count + 1;
+    				sum = sum + rewards2.get(j);
+    			}
+    		}
+    		qFunction[action][state] = sum/count;
+    	}
+    	List<Integer> temp = new ArrayList<Integer>();
+    	for(Integer state : states){
+    		if(!temp.contains(state)){
+    			temp.add(state);
+    			int maxIndex = 0;
+    	   	 	for (int a = 1; a < numActions; a++) {
+    	   	 		if (qFunction[a][state] > pi[maxIndex][state]) {
+    	   	 			maxIndex = a;
+    	   	 		}
+    	   	 	}
+    	         int a_star = maxIndex;
+    	         for(int i = 0; i < actions.size(); i++){
+    	        	 if(actions.get(i) == a_star){
+    	        		 pi[i][state] = 1-epsilon+(epsilon/numActions);
+    	        	 }else{
+    	        		 pi[i][state] = epsilon/numActions;
+    	        	 }
+    	         }
+    	         
+    		}
+    	}
+            /*  (c) For each s in the episode:
             *  	a* <- arg max_a Q(s,a)
             *  	For all a element A(s):
             *  				1-e+e/|a(s)| if a = a*
             *  	pi(s,a)
             *  				e/|A(s)|     if a != a*
             */
-    	 states = new ArrayList<Integer>();
-    	 actions = new ArrayList<Integer>();
-    	 rewards = new ArrayList<Double>();
-    	 this.episodecounter = 0;
+ 
     }
 
     /**
      * Release memory that is no longer required/used.
      */
     public void agent_cleanup() {
+    	System.out.println("clean");
         //alles auf null setzen
     }
 
@@ -182,6 +222,7 @@ public class P4Cm4nAgentSteffen implements AgentInterface {
      * @return
      */
     public String agent_message(String message) {
+    	//System.out.println("a_message");
     	if (message.equals("team name")) {
             return "P4Cm4n";
         }
