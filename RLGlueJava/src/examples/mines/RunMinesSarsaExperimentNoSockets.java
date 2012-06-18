21690c1c5a0f678faf3b2cd9a18b9ad6e7126584
Torben Woggan <torbenx@gmx.de>
2012-06-18 13:48:47 +0200

    added other agents to "NoSockets" experiment

--- RLGlueJava/src/examples/mines/RunMinesSarsaExperimentNoSockets.java
+++ RLGlueJava/src/examples/mines/RunMinesSarsaExperimentNoSockets.java
@@ -43,6 +43,8 @@ public class RunMinesSarsaExperimentNoSockets{
 	
 	public static void main(String[] args){
 		//Create the Agent
+		//AgentInterface theAgent=new SampleSarsaAgent();
+		//AgentInterface theAgent=new P4Cm4nAgentTorben();
 		AgentInterface theAgent=new P4Cm4nAgentSteffen();
 		
 		//Create the Environment
